3c1c7bf3aa80f370ea6f91fa8f6ad0279886f330
Monish K <113332378+192111003@users.noreply.github.com>
2025-01-02 20:59:29 +0530

    Create CopyStr.c

---
+++ CopyStr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char source[100], destination[100];
+    printf("Enter the source string: ");
+    scanf("%s", source);
+    int i = 0;
+    while (source[i] != '\0') {
+        destination[i] = source[i];
+        i++;
+    }
+    destination[i] = '\0';
+    printf("The copied string is: %s\n", destination);
+    return 0;
+}
